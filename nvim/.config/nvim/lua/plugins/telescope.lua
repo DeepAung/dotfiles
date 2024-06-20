6825041c7ce9ed0d7676ba9787eabc83996f200c
deepaung <i.deepaung@gmail.com>
2024-06-20 21:49:17 +0700

    feat: fix quick-fix-list

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,7 +7,9 @@ return {
     {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
-      cond = function() return vim.fn.executable("make") == 1 end,
+      cond = function()
+        return vim.fn.executable("make") == 1
+      end,
     },
     "nvim-telescope/telescope-ui-select.nvim",
     { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
@@ -19,8 +21,8 @@ return {
     require("telescope").setup({
       defaults = {
         mappings = {
-          i = { ["<C-q>"] = actions.smart_add_to_qflist + actions.open_qflist },
-          n = { ["<C-q>"] = actions.smart_add_to_qflist + actions.open_qflist },
+          i = { ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist },
+          n = { ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist },
         },
       },
       picker = {},
@@ -39,7 +41,9 @@ return {
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
     vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-    vim.keymap.set("n", "<leader>sf", function() builtin.find_files({ hidden = true }) end, { desc = "[S]earch [F]iles" })
+    vim.keymap.set("n", "<leader>sf", function()
+      builtin.find_files({ hidden = true })
+    end, { desc = "[S]earch [F]iles" })
     vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
     vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
     vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
@@ -50,7 +54,8 @@ return {
     -- vim.keymap.set("n", "<C-q>", actions.smart_add_to_qflist(), { desc = "Smart add to quick-fix-list" })
 
     -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set("n", "<leader>sn", function() builtin.find_files({ cwd = vim.fn.stdpath("config") }) end,
-      { desc = "[S]earch [N]eovim files" })
+    vim.keymap.set("n", "<leader>sn", function()
+      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    end, { desc = "[S]earch [N]eovim files" })
   end,
 }
