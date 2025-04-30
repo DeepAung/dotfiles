78b5f6b53dbd6550d1565baea8023c6c56f5081c
DeepAung <i.deepaung@gmail.com>
2025-04-30 20:58:48 +0700

    feat(nvim): add oil.nvim

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/oil.nvim',
+  config = function()
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+      },
+    }
+
+    vim.keymap.set('n', '-', ':Oil<cr>', { desc = 'Oil: open parent directory' })
+  end,
+  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+  lazy = false,
+}
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -50,7 +50,7 @@ return {
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader>s<leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>s<leader>', builtin.buffers, { desc = '[S]earch existing buffers' })
     vim.keymap.set('n', '<leader>sb', builtin.lsp_document_symbols, { desc = '[S]earch Sym[b]ols' })
 
     vim.keymap.set('n', '<leader>sp', function()
