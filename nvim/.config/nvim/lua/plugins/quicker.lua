29deb2339f882f1358960ebd21a7045c2d8f0358
DeepAung <i.deepaung@gmail.com>
2025-11-17 17:28:09 +0700

    Update nvim config

---
+++ nvim/.config/nvim/lua/plugins/quicker.lua
@@ -0,0 +1,19 @@
+return {
+  'stevearc/quicker.nvim',
+  ft = 'qf',
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {},
+  config = function()
+    local quicker = require 'quicker'
+
+    vim.keymap.set('n', '<leader>q', function()
+      quicker.toggle()
+    end, { desc = 'Toggle [Q]uickfix list' })
+    vim.keymap.set('n', '<leader>l', function()
+      quicker.toggle { loclist = true }
+    end, { desc = 'Toggle [L]oclist' })
+
+    quicker.setup()
+  end,
+}
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,8 @@ return {
     dependencies = {
       'vrischmann/tree-sitter-templ',
       'windwp/nvim-ts-autotag',
+      'nvim-treesitter/nvim-treesitter-context',
+      'nvim-treesitter/nvim-treesitter-textobjects',
     },
     build = ':TSUpdate',
     opts = {
@@ -12,10 +14,22 @@ return {
       highlight = { enable = true },
       indent = { enable = true },
       autotag = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['ac'] = '@class.outer',
+            ['ic'] = '@class.inner',
+          },
+          include_surrounding_whitespace = true,
+        },
+      },
     },
     config = function(_, opts)
       require('nvim-treesitter.configs').setup(opts)
     end,
   },
-  'nvim-treesitter/nvim-treesitter-context',
 }
